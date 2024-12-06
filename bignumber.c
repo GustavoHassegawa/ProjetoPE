ea539719e082b8149beac7283a219b3ef23389d9
Gustavo Jun Miyamoto Hassegawa <gustavohassegawa@gmail.com>
2024-12-05 21:26:23 -0300

    Update bignumber.c

--- bignumber.c
+++ bignumber.c
@@ -29,6 +29,22 @@ void add_digit(BigNumber number, int digit) {
     number->end = new_node;
 }
 
+//Adiciona um novo no ao bignumber no começo
+void add_digit_head(BigNumber number, int digit) {
+    Node new_node = malloc(sizeof(Node));
+
+    new_node->digit = digit;
+    new_node->prev = NULL;
+    new_node->next = number->begin;
+
+    if (number->begin != NULL) {
+        number->begin->prev = new_node;
+    } else {
+        number->end = new_node;
+    }
+    number->begin = new_node;
+}
+
 /*Retorna o dígito, se não for um número retorna '-1', visto que o sinal será
  *definido em outro momento, agora estou pensando apenas no valor do digito.*/
 int return_digit(char character) {
@@ -39,6 +55,7 @@ int return_digit(char character) {
 }
 
 //Cria um bignumber a partir de uma srting
+/* NÃO UTILIZADO
 BigNumber char_bignumber(char *string) {
     int zero_to_the_left = 1;
     BigNumber number = create_bignumber();
@@ -58,18 +75,15 @@ BigNumber char_bignumber(char *string) {
     }
     return number;
 }
+*/
 
 //Le o bignumber
 void read_bignumber(BigNumber number) {
     char character;
 
-    while ((character = getchar()) != '\n') {
-        if (character >= '0' && character <= '9') {
+    while ((character = getchar()) != '\n')
+        if (character >= '0' && character <= '9') 
             add_digit(number, return_digit(character));
-        } else {
-            printf("\nEntrada inválida! Apenas números são permitidos.\n");
-        }
-    }
 }
 
 //Imprime um bignumber
@@ -88,5 +102,15 @@ void erase_bignumber(BigNumber number) {
     while (currentNode != NULL) {
         free(currentNode);
         currentNode = currentNode->next;
+    }
+}
+
+BigNumber sum_bignumber(BigNumber number1, BigNumber number2) {
+    Node currentNode1 = number1->end, currentNode2 = number2->end;
+    int sum, sum_next_house = 0;
+
+    while (currentNode1 != NULL || currentNode2 != NULL) {
+        sum = currentNode1->digit + currentNode2->digit;
+
     }
 }
